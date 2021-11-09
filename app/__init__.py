1fd3bcb97c8bd871479f116e0d2653505db77927
Michalczyk <dawid.michalczyk@capgemini.com>
2021-11-10 00:07:55 +0100

    db created, errors to fields added

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,14 @@ The script simply creates the application object as an instance of class Flask i
 '''
 
 from flask import Flask
-from flask.config import Config
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'diaojwdoiajdoiw'
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
