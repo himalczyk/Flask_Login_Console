6c3a8972057dca54b66e9d6306c0c8ed8154b6b7
Michalczyk <dawid.michalczyk@capgemini.com>
2021-11-23 20:30:40 +0100

    added tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,18 @@
-'''
-The script simply creates the application object as an instance of class Flask imported from the flask package. The __name__ variable passed to the Flask class is a Python predefined variable, which is set to the name of the module in which it is used. Flask uses the location of the module passed here as a starting point when it needs to load associated resources such as template files
-'''
-
-from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
-
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'diaojwdoiajdoiw'
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-
-#for sending error via set up email server and account
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -31,27 +23,23 @@ if not app.debug:
         if app.config['MAIL_USE_TLS']:
             secure = ()
         mail_handler = SMTPHandler(
-            mailhost = (app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr = 'no_reply@' + app.config['MAIL_SERVER'],
-            toaddr = app.config['ADMINS'], subject = 'Microblog failure',
-            credentials = auth, secure=secure
-        )
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
-    
-    #for logging to File error handling
-        
+
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount = 10)
-    file_handler = setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
-    ))
-    file_handler.setLevel(logging.ERROR)
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
-    
+
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
 
 from app import routes, models, errors
-
